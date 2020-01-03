53f1c5ec30264f1cdc53268269cc195874ef5f18
cpmangukiya <chintanpmangukiya@gmail.com>
2020-01-03 10:38:15 +0530

    another commit

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping(path = "/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
