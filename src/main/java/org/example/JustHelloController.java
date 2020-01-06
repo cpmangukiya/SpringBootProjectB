fc91724db74b10907a968491e18880f3913b7af0
cpmangukiya <chintanpmangukiya@gmail.com>
2020-01-06 09:41:29 +0530

    another commit

--- src/main/java/org/example/JustHelloController.java
+++ src/main/java/org/example/JustHelloController.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
-public class Controller {
+public class JustHelloController {
+
+    Logger logger = (Logger) LoggerFactory.getLogger(JustHelloController.class);
 
     @RequestMapping(path = "/hello")
     public String hello() {
+        logger.info("Hello called up");
         return "Hello World";
     }
 
