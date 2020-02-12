2e477c03c7a318ccda4dce41e2fea33506b3ab40
ChintanMangukiya-OUP <ChintanMangukiya-OUP@github.com>
2020-02-12 15:16:21 +0530

    another commit

--- src/main/resources/db/migration/V1.0.0__init.sql
+++ src/main/resources/db/migration/V1.0.0__init.sql
@@ -1,4 +1,4 @@
-/*This step is necessary as flyway will not auto creata a table for you also method findByRepoName is necessary */
+/*This step is necessary as flyway will not auto create a table for you also method findByRepoName is necessary */
 CREATE TABLE GITHUB (
     id  IDENTITY NOT NULL PRIMARY KEY,
     org_name VARCHAR(50) NOT NULL,
